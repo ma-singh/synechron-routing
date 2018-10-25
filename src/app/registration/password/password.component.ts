f42349fe80b834ab5092bb2b9d506d061a828c7e
ma-singh <masingh@fullsail.edu>
2018-10-25 10:37:34 -0400

    changing project config to use SCSS styles

--- src/app/registration/password/password.component.ts
+++ src/app/registration/password/password.component.ts
@@ -4,7 +4,7 @@ import { FormBuilder, Validators } from '@angular/forms';
 @Component({
   selector: 'app-password',
   templateUrl: './password.component.html',
-  styleUrls: ['./password.component.css']
+  styleUrls: ['./password.component.scss']
 })
 export class PasswordComponent implements OnInit {
 
